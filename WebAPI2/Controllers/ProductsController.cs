79e606612d5e942b0979cc1508be1ba54642569d
antirix <30551791+antirix@users.noreply.github.com>
2022-05-05 18:48:04 +0300

    5.05.2022

---
+++ WebAPI2/Controllers/ProductsController.cs
@@ -0,0 +1,53 @@
+﻿using Business.Abstract;
+using Entities.Concreate;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebAPI2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        { 
+            _productService = productService; 
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll() 
+        {
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result =_productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+
+
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}            
