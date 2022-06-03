3787ee2e41ae24ec9b087d2618b9fc56603d7015
Kerem <kerem.yildiz@flokserkimya.com.tr>
2022-06-03 16:03:13 +0300

    adder

--- Business/Concreate/ProductManager.cs
+++ Business/Concreate/ProductManager.cs
@@ -3,6 +3,7 @@ using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcers.Validation;
+using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concreate;
@@ -10,6 +11,7 @@ using Entities.DTOs;
 using FluentValidation;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Business.Concreate
@@ -17,31 +19,43 @@ namespace Business.Concreate
     public class ProductManager : IProductService
     {
         IProductDal _productDal;
-        public ProductManager(IProductDal productDal)
+        ICategoryService _categoryService;
+        public ProductManager(IProductDal productDal,ICategoryService categoryService)
         {
             _productDal = productDal;
+            _categoryService = categoryService;
         }
 
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
-        { c
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  //business codes
+        {
+
+            //business codes
+
+            IResult result = BusinessRules.Run(ChechIfProductCountOfCategoryCorrect(product.CategoryId),
+                                               ChechIfProdutNameExists(product.ProductName),
+                                               CheckIfCategoryLimitExceded());
+
+            if (result != null)
+            {
+                return result;
+            }
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
         }
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour==25)
+            if (DateTime.Now.Hour == 25)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
-            return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductListed);
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductListed);
         }
 
         public IDataResult<List<Product>> GetAllByCategoryId(int id)
         {
-            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p=> p.CategoryId==id));
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.CategoryId == id));
         }
 
         public IDataResult<List<Product>> GetAllByUnitPrice(decimal min, decimal max)
@@ -51,12 +65,48 @@ namespace Business.Concreate
 
         public IDataResult<Product> GetById(int productId)
         {
-            return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId==productId));
+            return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
         }
 
         public IDataResult<List<ProductDetailDto>> GetProductDetails()
         {
             return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
         }
+
+        [ValidationAspect(typeof(ProductValidator))]
+        public IResult Update(Product product)
+        {
+            _productDal.Update(product);
+            return new SuccessResult(Messages.ProductUpdated);
+        }
+        private IResult ChechIfProductCountOfCategoryCorrect(int category)
+        {
+            var result = _productDal.GetAll(p => p.CategoryId == category).Count;
+            if (result >= 15)
+            {
+                return new ErrorResult(Messages.ProductCountOfCategoryError);
+            }
+            return new SuccessResult();
+        }
+        private IResult ChechIfProdutNameExists(string productName)
+        {
+            var result = _productDal.GetAll(p => p.ProductName == productName).Any();
+            if (result)
+            {
+                return new ErrorResult(Messages.ProdutNameAlreadyExist);
+            }
+            return new SuccessResult();
+        }
+        private IResult CheckIfCategoryLimitExceded()
+        {
+            var result = _categoryService.GetAll();
+            if (result.Data.Count > 15)
+            {
+                return new ErrorResult(Messages.CategoryLimixExcede);
+            }
+
+          return new SuccessResult();
+        }
+        
     }
 }
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -12,5 +12,10 @@ namespace Business.Constants
         public static string ProductUpdated = "Ürün güncellendi.";
         public static string MaintenanceTime = "Sistem bakımda.";
         public static string ProductListed = "Ürünler listelendi.";
+        public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir.";
+        public static string ProdutNameAlreadyExist = "Bu isim zaten var.";
+        public static string Succes = "Başarılı";
+        public static string Error = "Hata";
+        public static string CategoryLimixExcede = "Kategori limiti aşıldığı için yeni ürün eklenemiyor.";
     }
 }
